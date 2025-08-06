cf795f3b155a47dde4ef57371fe5d2d6ae8b6624
sudang <sudangjin@gmail.com>
2025-08-06 23:35:44 +0900

    [TurnDefence] Apply asset store resources

--- Assets/SPUM/Sample/Script/PlayerObj.cs
+++ Assets/SPUM/Sample/Script/PlayerObj.cs
@@ -1,39 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class PlayerObj : MonoBehaviour
 {
     public SPUM_Prefabs _prefabs;
     public float _charMS;
-    public enum PlayerState
-    {
-        idle,
-        move,
-        attack,
-        death,
-    }
-    public PlayerState _playerState = PlayerState.idle;
-    public Vector3 _goalPos;
-    // Start is called before the first frame update
+    private PlayerState _currentState;
 
-    // Update is called once per frame
+    public Vector3 _goalPos;
+    public bool isAction = false;
+    public Dictionary<PlayerState, int> IndexPair = new ();
     void Start()
     {
-
+        if(_prefabs == null )
+        {
+            _prefabs = transform.GetChild(0).GetComponent<SPUM_Prefabs>();
+            if(!_prefabs.allListsHaveItemsExist()){
+                _prefabs.PopulateAnimationLists();
+            }
+        }
+        _prefabs.OverrideControllerInit();
+        foreach (PlayerState state in Enum.GetValues(typeof(PlayerState)))
+        {
+            IndexPair[state] = 0;
+        }
+    }
+    public void SetStateAnimationIndex(PlayerState state, int index = 0){
+        IndexPair[state] = index;
+    }
+    public void PlayStateAnimation(PlayerState state){
+        _prefabs.PlayAnimation(state, IndexPair[state]);
     }
     void Update()
     {
+        if(isAction) return;
+
         transform.position = new Vector3(transform.position.x,transform.position.y,transform.localPosition.y * 0.01f);
-        switch(_playerState)
+        switch(_currentState)
         {
-            case PlayerState.idle:
+            case PlayerState.IDLE:
+            
             break;
 
-            case PlayerState.move:
+            case PlayerState.MOVE:
             DoMove();
             break;
         }
+        PlayStateAnimation(_currentState);
+
     }
 
     void DoMove()
@@ -42,12 +61,11 @@ public class PlayerObj : MonoBehaviour
         Vector3 _disVec = (Vector2)_goalPos - (Vector2)transform.position ;
         if( _disVec.sqrMagnitude < 0.1f )
         {
-            _prefabs.PlayAnimation(0);
-            _playerState = PlayerState.idle;
+            _currentState = PlayerState.IDLE;
             return;
         }
         Vector3 _dirMVec = _dirVec.normalized;
-        transform.position += (_dirMVec * _charMS * Time.deltaTime );
+        transform.position += _dirMVec * _charMS * Time.deltaTime;
         
 
         if(_dirMVec.x > 0 ) _prefabs.transform.localScale = new Vector3(-1,1,1);
@@ -56,8 +74,8 @@ public class PlayerObj : MonoBehaviour
 
     public void SetMovePos(Vector2 pos)
     {
+        isAction = false;
         _goalPos = pos;
-        _playerState = PlayerState.move;
-        _prefabs.PlayAnimation(1);
+        _currentState = PlayerState.MOVE;
     }
 }
